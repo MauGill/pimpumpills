e9554a2511c0faf417da08a3564d7b1aceff6866
MauGill <84589470+MauGill@users.noreply.github.com>
2022-02-23 09:54:13 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -6,6 +6,29 @@ navToggler.addEventListener("click", toggleNav);
 function toggleNav (){
     navToggler.classList.toggle("active")
     car.classList.toggle("active")
-    document.querySelector(".nav").classList.toggle("open");
+    document.querySelector(".nav").classList.toggle("open")
+    document.querySelector(".navbar").classList.add("sticky");
+
+    
 
 }
+
+/*                                                      close nav when clicking on a nav item */
+document.addEventListener("click", function(e){
+    if(e.target.closest(".nav-item")){
+        toggleNav();
+    }
+})
+/*                                                      Sticky header */
+window.addEventListener("scroll", function(){
+    if(this.pageYOffset > 100) {
+        document.querySelector(".navbar").classList.add("sticky");
+    } else {
+        document.querySelector(".navbar").classList.remove("sticky");
+    }
+    if(this.pageYOffset <= 0) {
+        document.querySelector(".nav").classList.remove("open")
+        document.querySelector(".nav-toggler").classList.remove("active")
+        document.querySelector(".car").classList.remove("active")
+    }
+})
