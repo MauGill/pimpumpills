2e4bd3f514308808a33336c00598c72193f248f1
MauGill <84589470+MauGill@users.noreply.github.com>
2022-03-08 16:46:55 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -50,3 +50,52 @@ window.addEventListener('load', function(){
       dots:'dots'
     })
   })
+/*                                                      Back to top btn */
+const topBtn = document.querySelector(".top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction ( ) {
+    if (window.pageYOffset > 300) { //Show btn
+
+        if(!topBtn.classList.contains("btnEntrance")) {
+            topBtn.classList.remove("btnExit");
+            topBtn.classList.add("btnEntrance");
+            topBtn.style.display = "block";
+          }
+        
+    } else { // Hide btn
+
+        if(topBtn.classList.contains("btnEntrance")) {
+            topBtn.classList.remove("btnEntrance");
+            topBtn.classList.add("btnExit");
+            setTimeout(function() {
+              topBtn.style.display = "none";
+            }, 250);
+          }
+        }
+      }
+topBtn.addEventListener("click", backToTop);
+
+function backToTop() {
+        const targetPosition = 0;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 750;
+        let start = null;
+        
+        window.requestAnimationFrame(step);
+      
+        function step(timestamp) {
+          if (!start) start = timestamp;
+          const progress = timestamp - start;
+          window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+          if (progress < duration) window.requestAnimationFrame(step);
+        }
+      }    
+      function easeInOutCubic(t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2*t*t*t + b;
+          t -= 2;
+          return c/2*(t*t*t + 2) + b;
+      };  
